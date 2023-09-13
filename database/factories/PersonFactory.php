a819ec74543ca83103092b70555b93fc0b79683a
Rangasamy <rangasamy.psg@gmail.com>
2023-09-13 18:07:28 +0530

    first commit

---
+++ database/factories/PersonFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PersonFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone' => $this->faker->numerify('##########')
+        ];
+    }
+}
